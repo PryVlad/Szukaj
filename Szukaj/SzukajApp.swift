bebbe32a61ff48da3e6014df694b73f5956075f3
PryVlad <pr.vl@outlook.com>
2024-12-15 07:49:36 +0100

    Initial Commit

---
+++ Szukaj/SzukajApp.swift
@@ -0,0 +1,17 @@
+//
+//  SzukajApp.swift
+//  Szukaj
+//
+//  Created by Vladyslav Pryl on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SzukajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

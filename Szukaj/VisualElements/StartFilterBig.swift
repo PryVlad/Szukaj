89a1bee91c389df92f609a62a2d79ca691027a13
PryVlad <pr.vl@outlook.com>
2024-12-20 16:05:02 +0100

    Two filter buttons, horizontal background fix and more.

---
+++ Szukaj/VisualElements/StartFilterBig.swift
@@ -0,0 +1,18 @@
+//
+//  StartFilterBig.swift
+//  Szukaj
+//
+//  Created by Vladyslav Pryl on 20.12.2024.
+//
+
+import SwiftUI
+
+struct StartFilterBig: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartFilterBig()
+}
